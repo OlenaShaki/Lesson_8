a352be3fa8ce90e01c044c761155767291b9a485
Olena Shakirova <Ellandy6@gmail.com>
2023-06-03 19:37:18 +0300

    add yslovie 3

--- mane.py
+++ mane.py
@@ -1,9 +1,11 @@
 def mane_function(s):
     if s == "":
         print("Hello, world!")
-    elif s >= 10:
-        print("Hello, my friend!")
+    elif s > 10:
+        print("Hello, my big friend!")
+    elif s <= 10:
+        print("Hello, my little friend!")
     else:
         print(f"Hello, {s}!")
 
-mane_function(10)
+mane_function(11)
