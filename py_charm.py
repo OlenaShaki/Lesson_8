e97c37a90e3b564b468c9636c76f8aba9eed6ce5
Olena Shakirova <Ellandy6@gmail.com>
2023-06-03 21:33:14 +0300

    first commit

---
+++ py_charm.py
@@ -0,0 +1,14 @@
+def pora_roky02():
+    s = int (input("Введіть номер місяця: "))
+    if s == 12 or s== 1 or s== 2 :
+        print("Це зима")
+    elif s == 3 or s== 4 or s== 5 :
+        print("Це весна")
+    elif s == 6 or s== 7 or s== 8 :
+        print("Це літо")
+    elif s == 9 or s== 10 or s== 11 :
+        print("Це осінь")
+    elif s > 13:
+        print("Введіть число від 1 до 12")
+
+pora_roky()
