5a17eb5b51855a54f6a9ba07c28cd98df06a6c16
Olena Shakirova <Ellandy6@gmail.com>
2023-06-03 21:38:14 +0300

    add elif 3, 4, 5

--- py_charm.py
+++ py_charm.py
@@ -2,12 +2,16 @@ def pora_roky02():
     s = int (input("Введіть номер місяця: "))
     if s == 12 or s== 1 or s== 2 :
         print("Це зима, місяць грудень")
-    elif s == 1
+    elif s == 1:
         print("Це зима, місяць січень")
-    elif s == 2
+    elif s == 2:
         print("Це зима, місяць лютий")
-    elif s == 3 or s== 4 or s== 5 :
-        print("Це весна")
+    elif s == 3:
+        print("Це весна, місяць березень")
+    elif s == 4:
+        print("Це зима, місяць квітень")
+    elif s == 5:
+        print("Це зима, місяць травень")
     elif s == 6 or s== 7 or s== 8 :
         print("Це літо")
     elif s == 9 or s== 10 or s== 11 :
